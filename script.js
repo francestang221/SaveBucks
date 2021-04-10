0f52f128e513ae358d15c58cdcc848d36670c50d
Rachel Peterson <rachel.peterson.5683@gmail.com>
2021-04-10 13:06:06 -0600

    updated formatting, added gif

--- script.js
+++ script.js
@@ -30,9 +30,6 @@ function calcTotalValue(year, daily_invest){
     return total_value.toFixed(2);
 }
 
-
-
-
 // creating elements for outputs
 const coffee_price = document.getElementById('coffee-price');
 let p1 = document.createElement('p')
@@ -87,19 +84,18 @@ document.getElementById('button-enter').onclick = function getSavings() {
                     sb_price = coffees.price; 
                     console.log(sb_price);
 
-                    // show the requested drink's price
-                    p1.textContent = 'Your go-to drink is a ' + coffee_name + '. ' + 'YUM! \r\n'
-                    p1.textContent += 'The price of a ' + sb_price + ' is ~$'
+                    // show the requested drink's prices
+                    p1.textContent = 'The price of a grande ' + coffee_name + ' from Starbucks is ~$'
                     p1.textContent += sb_price;
                     p1.textContent += '. '
 
                     // show daily savings
-                    var daily_cost = sb_price * qty;
+                    var daily_cost = (sb_price * qty).toFixed(2);
                     p2.textContent = 'Your coffee habit is costing you $' + daily_cost + ' per day! \r\n'
                 
             
                     // add styling to new html content
-                    let newStyle = document.querySelector('#savings');
+                    let newStyle = document.querySelector('#results');
                     newStyle.classList.add("output");
 
                     // show total potential investment 
@@ -110,15 +106,17 @@ document.getElementById('button-enter').onclick = function getSavings() {
                     p3.textContent += 'Creamer: $0.03 \r\n'
                     p3.textContent += 'Total= $0.20 per cup \r\n'
                     p3.textContent += '\r\n'
+
                     // calculate the savings per cup
                     const savings_per_cup = (sb_price - 0.20).toFixed(2);
                     p3.textContent += 'Having your coffee at home would save you $' + savings_per_cup + ' per cup! \r\n'
                     const daily_savings = (qty*savings_per_cup).toFixed(2)
-                    p3.textContent += '$' + savings_per_cup + ' per cup X ' + qty + ' cups per day = $' + daily_savings + ' saved per day. \r\n'
+                    p3.textContent += 'At $' + savings_per_cup + ' per cup and ' + qty + ' cups per day, that is $' + daily_savings + ' saved per day! \r\n'
                     p3.textContent += '\r\nIf you instead invested that $' + daily_savings + ' per day:\r\n'
                     var total_val = addCommas(calcTotalValue(55, parseInt(daily_savings)));
                     p3.textContent += 'After 55 years at a 7% return, \r\n'
                     p3.textContent += 'you would have $' + total_val + '!\r\n'
+                    document.getElementById('results').innerHTML += `<img style="margin:20px; width:500px; box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);" src="woah.gif" alt="Bill Nye is amazed gif"/>`;
                 }           
             });
         })
